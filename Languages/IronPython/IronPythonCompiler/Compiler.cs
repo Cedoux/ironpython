ae6c265965205350ea787faea5e3e3a93fb9814d
Jeff Hardy <jdhardy@gmail.com>
2012-09-03 00:21:09 -0700

    Add project for ipyc.exe.

---
+++ Languages/IronPython/IronPythonCompiler/Compiler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronPythonCompiler {
+    class Compiler {
+        static void Main(string[] args) {
+        }
+    }
+}
