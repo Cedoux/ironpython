90fbcdf9eef4d45d6b7f8ee5b2d4ebd3eafc3b90
Jeff Hardy <jdhardy@gmail.com>
2012-10-05 00:45:08 -0700

    Remove hardcoded assembly names.
    Instead of hardcoding references, search a list of given paths for the
    appropriate assembly. Also automatically shift type references around,
    and re-namespace the handful of types that need it.

--- Languages/IronPython/Samples/ClrType/rewrite.py
+++ Languages/IronPython/Samples/ClrType/rewrite.py
@@ -12,104 +12,100 @@ from Mono.Cecil import *
 from Mono.Cecil.Cil import OpCodes
 
 class Platform(object):
-    default_references = [
-        'mscorlib', 'System', 'System.Core'
-    ]
-    
     platform_references = []
     
-    def __init__(self, refpath, user_references):
+    def __init__(self, user_reference_paths=[], user_references=[]):
         self._reference_map = {}
         self._type_map = {}
-        for ref in itertools.chain(self.default_references, self.platform_references, user_references):
-            dll = os.path.join(refpath, ref + '.dll')
-            assy = AssemblyDefinition.ReadAssembly(dll)
-            self._reference_map[assy.Name.Name] = assy
-            
-            for mod in assy.Modules:
-                for t in mod.GetTypes():
-                    self._type_map[t.FullName] = t
+        
+        self._all_reference_paths = self.reference_paths + user_reference_paths
+        self._all_references = set(self.platform_references + user_references)
         
         from pprint import pprint
-        pprint(self._reference_map)
-        pprint(self._type_map)
+        # pprint(self._all_reference_paths)
+        # pprint(self._type_map)
     
     def rewrite_il(self, mod):
         pass
     
     def update_references(self, mod):
-        for t in mod.GetTypeReferences():
-            try:
-                t.Scope = self._type_map[t.FullName].Scope
-            except KeyError:
-                print 'Skipping', t.FullName
-                pass
+        mod.Runtime = self.runtime
+        existing_references = set()
         
         for ref in mod.AssemblyReferences:
-            try:
-                ref.Version = self._reference_map[ref.Name].Name.Version
-            except KeyError:
-                print 'Skipping', ref.Name
-                pass
+            existing_references.add(ref.Name)
+            ref.Version = self._find_reference_assembly(ref.Name).Name.Version
         
-        mod.Runtime = self.runtime
+        for missing_ref in self._all_references - existing_references:
+            assy = self._find_reference_assembly(missing_ref)
+            mod.AssemblyReferences.Add(assy.Name)
+        
+        for t in mod.GetTypeReferences():
+            t.Scope = self._find_type(t.FullName).Scope
     
-    def clean_attributes(self, assy):
-        pass
-
-class Net40(Platform):
+    def _find_reference_assembly(self, assy_name):
+        if assy_name not in self._reference_map:
+            assy_file = assy_name + '.dll'
+            for path in self._all_reference_paths:
+                assy_path = os.path.join(path, assy_file)
+                if os.path.exists(assy_path):
+                    assy = AssemblyDefinition.ReadAssembly(assy_path)
+                    self._reference_map[assy_name] = assy
+                    break
+        
+        return self._reference_map[assy_name]
+    
+    def _find_type(self, type_name):
+        if not self._type_map:
+            for assy in self._reference_map.values():
+                for mod in assy.Modules:
+                    for t in mod.GetTypes():
+                        self._type_map[t.FullName] = t
+        
+        return self._type_map[type_name]
+    
+class v4Platform(Platform):
     runtime = TargetRuntime.Net_4_0
-    extra_references = ['System.Numeric']
+    platform_references = ['System.Numeric']
 
-class Net20(Platform):
+class Net40(v4Platform):
+    reference_paths = [r'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0']
+
+class v2Platform(Platform):
     runtime = TargetRuntime.Net_2_0
+    platform_references = ['Microsoft.Scripting', 'Microsoft.Scripting.Core']
+
+class Net20(v2Platform):
+    reference_paths = [r'C:\Windows\Microsoft.NET\Framework\v2.0.50727',
+        r'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\v3.0',
+        r'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\v3.5']
     
     def rewrite_il(self, mod):
         fix_strongbox_ctor(mod)
     
     def update_references(self, mod):
-        super(Net20, self).update_references(mod)
-        
-        msc = AssemblyNameReference.Parse('Microsoft.Scripting.Core, Version=1.1.0.30, Culture=neutral, PublicKeyToken=7f709c5b713576e1')
-        mod.AssemblyReferences.Add(msc)
-
         for t in mod.GetTypeReferences():
-            n = t.FullName
-            if n.startswith('System.Dynamic.'):
-                t.Scope = msc
-            elif n.startswith('System.Linq.Expressions.'):
-                t.Scope = msc
+            if t.FullName.startswith('System.Linq.Expressions.'):
                 t.Namespace = 'Microsoft.Scripting.Ast'
-            elif (n.startswith('System.Runtime.CompilerServices.CallSite') or n.startswith('System.Runtime.CompilerServices.Closure')):
-                t.Scope = msc
-            elif n.startswith('System.Func'):
-                _, num = n.split('`')
+            elif t.FullName.startswith('System.Func'):
+                _, num = t.FullName.split('`')
                 try:
                     if int(num) > 5:
-                        t.Scope = msc
                         t.Namespace = 'Microsoft.Scripting.Utils'
                 except ValueError:
                     pass
+                
+        super(Net20, self).update_references(mod)
     
-    def clean_attributes(self, assy):
-        to_remove = []
-        for attrib in assy.CustomAttributes:
-            if attrib.AttributeType.FullName == 'System.Security.SecurityRulesAttribute':
-                to_remove.append(attrib)
-        
-        for dead in to_remove:
-            assy.CustomAttributes.Remove(dead)
+class Silverlight5(v4Platform):
+    reference_paths = [r'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\Silverlight\v5.0']
 
-class Silverlight5(Platform):
-    runtime = TargetRuntime.Net_4_0
-    extra_references = ['System.Numeric']
+class WinRT(v4Platform):
+    reference_paths = [r'']
 
-class WinRT(Platform):
-    runtime = TargetRuntime.Net_4_0
-
-class Android(Platform):
-    runtime = TargetRuntime.Net_2_0
-    extra_references = ['System.Numeric']
+class Android(v2Platform):
+    platform_references = v2Platform.platform_references + ['System.Numeric']
+    reference_paths = [r'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\MonoAndroid\v1.0']
 
 def fix_strongbox_ctor(mod):
     dcc = mod.GetType('DLRCachedCode')
@@ -117,14 +113,16 @@ def fix_strongbox_ctor(mod):
     strongbox_ctor_v2 = clr.GetClrType(System.Runtime.CompilerServices.StrongBox).MakeGenericType(System.Array[object]).GetConstructor((System.Array[object],))
     strongbox_ctor_v4 = clr.GetClrType(System.Runtime.CompilerServices.StrongBox).MakeGenericType(System.Array[object]).GetConstructor(System.Type.EmptyTypes)
 
-    #for method in dcc.Methods:
-    #    for instr in method.Body.Instructions:
+    #~ for method in dcc.Methods:
+        #~ il = method.Body.GetILProcessor()
+        #~ print method.Name
+        #~ for instr in method.Body.Instructions:
             #~ if instr.OpCode == OpCodes.Newobj:
                 #~ if instr.Operand.FullName == method.Module.Import(strongbox_ctor_v4).FullName:
-                    #~ il = method.Body.GetILProcessor()
                     #~ il.InsertBefore(instr, il.Create(OpCodes.Ldnull))
                     #~ il.Replace(instr, il.Create(OpCodes.Newobj, method.Module.Import(strongbox_ctor_v2)))
 
+    # This is super-fragile, but the above loop is not working
     method = dcc.Methods[0]
     il = method.Body.GetILProcessor()
     instr = method.Body.Instructions[4]
@@ -132,42 +130,13 @@ def fix_strongbox_ctor(mod):
     il.InsertBefore(instr, il.Create(OpCodes.Ldnull))
     il.Replace(instr, il.Create(OpCodes.Newobj, method.Module.Import(strongbox_ctor_v2)))
 
-def change_references(mod):
-    mod.Runtime = TargetRuntime.Net_2_0
-    refs = mod.AssemblyReferences
-    refs[0].Version = System.Version(2,0,0,0)
-    refs[3].Version = System.Version(2,0,0,0)
-    refs[5].Version = System.Version(2,0,0,0)
-    refs[2].Version = System.Version(3,5,0,0)
-
-    sc = refs[2]
-    msc = AssemblyNameReference.Parse('Microsoft.Scripting.Core, Version=1.1.0.30, Culture=neutral, PublicKeyToken=7f709c5b713576e1')
-    refs.Add(msc)
-
-    for t in ad.Modules[0].GetTypeReferences():
-        n = t.FullName
-        if n.startswith('System.Dynamic.'):
-            t.Scope = msc
-        elif n.startswith('System.Linq.Expressions.'):
-            t.Scope = msc
-            t.Namespace = 'Microsoft.Scripting.Ast'
-        elif (n.startswith('System.Runtime.CompilerServices.CallSite') or n.startswith('System.Runtime.CompilerServices.Closure')):
-            t.Scope = msc
-        elif n.startswith('System.Func'):
-            # moved from System.Core in 3.5 to mscorlib in 4
-            t.Scope = sc
-            if (n.startswith('System.Func`6') or n.startswith('System.Func`7') or n.startswith('System.Func`8')):
-                t.Scope = msc
-                t.Namespace = 'Microsoft.Scripting.Utils'
-
 def main():
     assy_file = sys.argv[1]
-    p = Net20(r'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v3.5\Profile\Client', [])
+    p = Net20([os.path.join(os.environ['DLR_ROOT'], r'bin\v2Debug')])
     
     assy = AssemblyDefinition.ReadAssembly(assy_file)
     p.rewrite_il(assy.Modules[0])
     p.update_references(assy.Modules[0])
-    p.clean_attributes(assy)
     
     assy.Write('out.dll')
     
