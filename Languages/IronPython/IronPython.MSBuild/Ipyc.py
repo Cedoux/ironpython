d6185ef955ac78cf37f87405921b216b734500de
Jeff Hardy <jdhardy@gmail.com>
2012-10-14 01:59:35 -0700

    Add IronPython.MSBuild.
    IronPython.MSBuild contains the Ipyc task for building apps with ipyc.
    The neat thing is that the MSBuild task is written in IronPython and
    compiled with ipyc, to make sure that plugin libraries actually work.

---
+++ Languages/IronPython/IronPython.MSBuild/Ipyc.py
@@ -0,0 +1,12 @@
+﻿import clr
+clr.AddReference("Microsoft.Build.Utilities.v4.0")
+
+from Microsoft.Build.Utilities import Task
+
+class Ipyc(Task):
+    __metaclass__ = clr.ClrClass
+    __clr_namespace__ = "IronPython.MSBuild"
+
+    def Execute(self):
+        self.Log.LogMessage(MessageImportance.High, "Hello, world");
+        return True
