10b3b9bc621de71a7f2f77851ad248211a965e88
Jeff Hardy <jdhardy@gmail.com>
2012-10-22 23:38:21 -0700

    Add "Target" property to Ipyc task.

--- Languages/IronPython/IronPython.MSBuild/Ipyc.py
+++ Languages/IronPython/IronPython.MSBuild/Ipyc.py
@@ -5,12 +5,28 @@ clr.AddReference("Microsoft.Build.Utilities.v4.0")
 from System.Diagnostics import Debugger
 
 from Microsoft.Build.Utilities import Task
-from Microsoft.Build.Framework import MessageImportance
+from Microsoft.Build.Framework import MessageImportance, RequiredAttribute
+
+Required = clr.wrap_attribute(RequiredAttribute)
 
 class Ipyc(Task):
     __metaclass__ = clr.ClrClass
     __clr_namespace__ = "IronPython.MSBuild"
 
+    def __init__(self):
+       self._target = "dll"
+
+    @clr.property(str)
+    @clr.attributes([Required])
+    def Target(self):
+        return self._target
+    
+    @Target.setter
+    def Target(self, value):
+        self._target = value
+
     def Execute(self):
-        self.Log.LogMessage(MessageImportance.High, "Hello, world");
+        cmd = ["ipyc.exe"]
+        cmd += ["/target:{}".format(self._target)]
+        self.Log.LogMessage(MessageImportance.High, " ".join(cmd))
         return True
