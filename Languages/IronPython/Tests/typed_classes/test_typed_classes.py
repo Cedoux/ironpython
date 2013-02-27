d77c947eff207b6ad6dbebf01ccdd3ef9e992bf6
Jeff Hardy <jdhardy@gmail.com>
2013-02-26 21:57:17 -0800

    Delete built test files to ensure they are rebuilt.

--- Languages/IronPython/Tests/typed_classes/test_typed_classes.py
+++ Languages/IronPython/Tests/typed_classes/test_typed_classes.py
@@ -1,19 +1,35 @@
+import os
+import sys
 
 from iptest.assert_util import *
 from iptest.process_util import *
 
 def build_test_dll():
-    result = launch_ironpython('-S',
-        '-c',
-        '"import clr;'
-        "clr.CompileModules('typed_class.dll', 'typed_class.py', compileTypes=True)\""
+    try:
+        os.unlink('typed_class.dll')
+    except OSError:
+        pass
+    
+    result = run_tool(sys.executable,
+        '-S -c "import clr; clr.CompileModules(\'typed_class.dll\', \'typed_class.py\', compileTypes=True)"'
     )
+    
+    #~ result = launch_ironpython('-S',
+        #~ '-c',
+        #~ '"import clr;'
+        #~ "clr.CompileModules('typed_class.dll', 'typed_class.py', compileTypes=True)\""
+    #~ )
     
     if result != 0:
         raise Exception
 
 def build_test_exe():
-    result = run_csc("/t:exe /r:IronPython.dll "
+    try:
+        os.unlink('TestTypedClass.exe')
+    except OSError:
+        pass
+    
+    result = run_csc("/t:exe /debug /r:IronPython.dll "
         "/r:Microsoft.Dynamic.dll /r:Microsoft.Scripting.dll "
         "/r:typed_class.dll TestTypedClass.cs"
     )
